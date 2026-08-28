30ea771f6b9663a9569924e8d420f3295ecb3cdb
RamSChoudhary <ramrit10@gmail.com>
2026-08-28 15:23:19 +0530

    Added config file

--- cmd/apis/main.go
+++ cmd/apis/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/GoLang/APIs/internal/config"
+)
 
 func main() {
+
 	fmt.Println("First apis in GoLang")
+
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to go lang apis"))
+	})
+	// set up server
+
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("Failed to start server")
+	}
+
+	fmt.Println("server started")
 }
